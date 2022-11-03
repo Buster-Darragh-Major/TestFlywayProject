994c6dd29f953f678dc6c47ef67b79fbf85a2e3a
Buster Major <Buster.Major@red-gate.com>
2022-11-03 14:32:54 +0000

    Update demo to more complex project

---
+++ schema-model/Tables/dbo.User.sql
@@ -0,0 +1,27 @@
+CREATE TABLE [dbo].[User]
+(
+[UserId] [int] NOT NULL IDENTITY(1, 1),
+[LastName] [nvarchar] (200) NULL,
+[Username] [nvarchar] (200) NULL,
+[Reference] [nvarchar] (50) NULL,
+[Reference1] [nvarchar] (50) NULL,
+[Reference2] [nvarchar] (50) NULL,
+[Reference3] [nvarchar] (50) NULL,
+[Reference4] [nvarchar] (50) NULL,
+[Reference5] [nvarchar] (50) NULL,
+[Reference7] [nvarchar] (50) NULL,
+[Reference8] [nvarchar] (50) NULL,
+[Reference9] [nvarchar] (50) NULL,
+[Reference10] [nvarchar] (50) NULL,
+[Reference11] [nvarchar] (50) NULL,
+[Reference12] [nvarchar] (50) NULL,
+[Reference13] [nvarchar] (50) NULL,
+[Reference14] [nvarchar] (50) NULL,
+[Reference15] [nvarchar] (50) NULL,
+[Reference16] [nvarchar] (50) NULL,
+[Reference17] [nvarchar] (50) NULL,
+[Reference19] [nvarchar] (50) NULL,
+[Reference20] [nvarchar] (50) NULL,
+[Reference2021] [nvarchar] (50) NULL
+)
+GO
