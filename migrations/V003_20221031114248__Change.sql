395c5e4a350529704360402256693d01ea8e6de7
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2022-12-08 10:42:37 +0000

    Update V003_20221031114248__Change.sql
    g

--- migrations/V003_20221031114248__Change.sql
+++ migrations/V003_20221031114248__Change.sql
@@ -4,8 +4,8 @@ SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDE
 GO
 PRINT N'Altering [dbo].[User]'
 GO
--- Some different commentffffff
--- Some comment about things yea
+-- Some different comment
+-- Some comment about 
 ALTER TABLE [dbo].[User] ADD
 [Username] [nvarchar] (200) NULL
 GO
