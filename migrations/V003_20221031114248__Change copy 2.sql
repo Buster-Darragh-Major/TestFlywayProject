afa4bc5c6865f377c46f594f5ebde906469b056e
Buster Major <buster.major@red-gate.com>
2023-11-03 12:11:06 +0000

    fgesg

---
+++ migrations/V003_20221031114248__Change copy 2.sql
@@ -0,0 +1,13 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Altering [dbo].[User]'
+GO
+-- Some different comment
+-- Some comment about 
+ALTER TABLE [dbo].[User] ADD
+[Username] [nvarchar] (200) NULL
+GO
+--fdhib 
+-- bvdfchusbfhurvbug
