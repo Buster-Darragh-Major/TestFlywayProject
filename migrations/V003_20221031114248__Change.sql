0f6c3fa412b58312697f73cdf4503d0b6a4e9615
Buster Major <buster.major@red-gate.com>
2023-12-04 14:37:28 +0000

    diff and rename

--- migrations/V003_20221031114248__Change.sql
+++ migrations/V003_20221031114248__Change.sql
@@ -5,7 +5,7 @@ GO
 PRINT N'Altering [dbo].[User]'
 GO
 -- Some different commentfdsafdsaf
--- Some comment about 
+-- Some comment about fefgrtehgtregfed
 ALTER TABLE [dbo].[User] ADD
 [Username] [nvarchar] (200) NULL
 GO
