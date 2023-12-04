cf6e1e2b52fe00059e89c9ef780f626fadabbe68
Buster Major <buster.major@red-gate.com>
2023-12-04 14:25:13 +0000

    two changes

--- migrations/V003_20221031114248__Change.sql
+++ migrations/V003_20221031114248__Change.sql
@@ -4,10 +4,10 @@ SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDE
 GO
 PRINT N'Altering [dbo].[User]'
 GO
--- Some different comment
+-- Some different commentfdsafdsaf
 -- Some comment about 
 ALTER TABLE [dbo].[User] ADD
 [Username] [nvarchar] (200) NULL
 GO
 --fdhib 
--- bvdfchusbfhurvbug bdhbfd
+-- bvdfchusbfhurvbug bdhbfddsafdsaf
