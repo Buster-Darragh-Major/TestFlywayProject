346fcfa7e57302f51bcdef8cc139200ff9e3856c
Buster Major <buster.major@red-gate.com>
2025-01-20 14:41:15 +0000

    commit some utf 16

---
+++ schema-model/Data/dbo.NewTable_Data.sql
@@ -0,0 +1,3 @@
+SET IDENTITY_INSERT [dbo].[NewTable] ON
+INSERT INTO [dbo].[NewTable] ([Id], [column_2], [column_1]) VALUES (1, 2, 1)
+SET IDENTITY_INSERT [dbo].[NewTable] OFF
--- schema-model/Tables/dbo.NewTable.sql
+++ schema-model/Tables/dbo.NewTable.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[NewTable]
 (
 [Id] [int] NOT NULL IDENTITY(1, 1),
-[column_1] [int] NULL
+[column_1] [int] NULL,
+[column_2] [int] NULL
 )
 GO
 ALTER TABLE [dbo].[NewTable] ADD CONSTRAINT [PK_NewTable] PRIMARY KEY CLUSTERED ([Id])
