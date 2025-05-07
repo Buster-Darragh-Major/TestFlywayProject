66f0464eaae326c33a78eedae0200928125a14be
Buster Major <buster.major@red-gate.com>
2025-05-07 10:20:15 +0100

    Add header

--- schema-model/Tables/dbo.Customers.sql
+++ schema-model/Tables/dbo.Customers.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[Customers]
 [col1] [int] NULL,
 [col2] [int] NULL,
 [id_ref] [int] NULL,
-[foreign_ref] nvarchar(20)
+[foreign_ref] [nvarchar] (20) NULL,
+[header] [nvarchar] (200) NULL
 )
 GO
